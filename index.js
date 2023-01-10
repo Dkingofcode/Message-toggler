db99c4f0a29dfba14e53cae071b4082108c75a9e
Dkingofcode <doladepo128@gmail.com>
2023-01-10 22:24:10 +0000

    Markup added

---
+++ index.js
@@ -0,0 +1,26 @@
+const { text } = require("express");
+
+const firstBox = document.querySelector(".text-one");
+const secondBox = document.querySelector(".text-two");
+const threeBox = document.querySelector(".text-three");
+const Button = document.querySelector(".btn");
+
+
+
+Button.addEventListener("click", () => {
+    text.classList.add("block");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
